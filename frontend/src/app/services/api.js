2e98e72da75bc0f9cfa61355e3bc9319577bf3d5
Bishal Adhikari <bishalgithub707@gmail.com>
2026-07-20 19:04:59 -0700

    updata

--- frontend/src/app/services/api.js
+++ frontend/src/app/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://fully-ecommerce-1.onrender.com/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default api;
